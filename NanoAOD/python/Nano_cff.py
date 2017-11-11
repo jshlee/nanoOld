dcf6499a6a117a270fa615277dd4f36b3cc33e63
Jason Lee <jason.lee@cern.ch>
2017-11-12 06:51:59 +0900

    rm customiseNano_cff.py

--- NanoAOD/python/Nano_cff.py
+++ NanoAOD/python/Nano_cff.py
@@ -4,4 +4,6 @@ def customise(process):
     process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
     process.load('cat.NanoAOD.cmesons_cff')
     process.nanoAOD_step += process.cmesonTables
+    process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+    process.MessageLogger.cerr.FwkSummary.reportEvery = cms.untracked.int32(1000)
     return(process)
