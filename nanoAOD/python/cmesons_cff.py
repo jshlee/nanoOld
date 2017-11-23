bca8f5149b84f8b1d6008f12c2e25d5851378617
Jason Lee <jason.lee@cern.ch>
2017-11-23 17:28:21 +0900

    add trackerMuon change cmeson

--- nanoAOD/python/cmesons_cff.py
+++ nanoAOD/python/cmesons_cff.py
@@ -2,16 +2,40 @@ import FWCore.ParameterSet.Config as cms
 from  PhysicsTools.NanoAOD.common_cff import *
 
 ##################### Tables for final output and docs ##########################
-
 cmesonTable = cms.EDProducer("CMesonProducer",
   jetLabel = cms.InputTag("slimmedJets"),
   vertexLabel = cms.InputTag("offlineSlimmedPrimaryVertices"),
   #mcLabel  = cms.InputTag("prunedGenParticles"),
-  maxNumPFCand = cms.int32(9999),
-  maxDeltaR = cms.double(0.2),
-  d0MassCut = cms.double(0.5),
-  d0MassWindow = cms.double(0.05),
-  applySoftLeptonCut = cms.bool(True)
+  applySoftLeptonCut = cms.bool(True),
+  # -- cuts on initial track collection --
+  # Track normalized Chi2 <
+  tkChi2Cut = cms.double(10.),
+  # Number of valid hits on track >=
+  tkNHitsCut = cms.int32(3),
+  # Pt of track >
+  tkPtCut = cms.double(0.35),
+  # Track impact parameter significance >
+  tkIPSigXYCut = cms.double(2.),
+  tkIPSigZCut = cms.double(-1.),
+  
+  # -- cuts on the vertex --
+  # Vertex chi2 <
+  vtxChi2Cut = cms.double(6.63),
+  # XY decay distance significance >
+  vtxDecaySigXYCut = cms.double(15.),
+  # XYZ decay distance significance >
+  vtxDecaySigXYZCut = cms.double(-1.),
+  
+  # -- miscellaneous cuts --
+  # POCA distance between tracks <
+  tkDCACut = cms.double(1.),
+  # check if either track has a hit radially inside the vertex position minus this number times the sigma of the vertex fit
+  # note: Set this to -1 to disable this cut, which MUST be done if you want to run V0Producer on the AOD track collection!
+  innerHitPosCut = cms.double(4.),
+  # cos(angleXY) between x and p of V0 candidate >
+  cosThetaXYCut = cms.double(0.998),
+  # cos(angleXYZ) between x and p of V0 candidate >
+  cosThetaXYZCut = cms.double(-2.),
 )
 
 cmesonCandidateTable =  cms.EDProducer("SimpleCandidateFlatTableProducer",
@@ -24,8 +48,8 @@ cmesonCandidateTable =  cms.EDProducer("SimpleCandidateFlatTableProducer",
         x   = Var("vx()", float, doc = "secondary vertex X position, in cm",precision=10),
         y   = Var("vy()", float, doc = "secondary vertex Y position, in cm",precision=10),
         z   = Var("vz()", float, doc = "secondary vertex Z position, in cm",precision=14),
-        ndof   = Var("vertexNdof()", float, doc = "number of degrees of freedom",precision=8),
-        chi2   = Var("vertexNormalizedChi2()", float, doc = "reduced chi2, i.e. chi/ndof",precision=8),
+        ndof= Var("vertexNdof()", float, doc = "number of degrees of freedom",precision=8),
+        chi2= Var("vertexNormalizedChi2()", float, doc = "reduced chi2, i.e. chi/ndof",precision=8),
     ),
 )
 cmesonCandidateTable.variables.pt.precision=10
