0902aae0b2b72baba358d6ee9ee044fcbd2323d5
yechan <yck.kang@gmail.com>
2018-01-26 17:37:59 +0900

    Update Json reader

--- analysis/h2mu/nanoAnalysis.cc
+++ analysis/h2mu/nanoAnalysis.cc
@@ -13,27 +13,31 @@ g++ `root-config --cflags --glibs` nanoAnalysis.cc -o nanoAnalysis
 
 void nanoAnalysis::Loop()
 {
-   if (fChain == 0) return;
+  if (fChain == 0) return;
 
-   Long64_t nentries = fChain->GetEntriesFast();
+  Long64_t nentries = fChain->GetEntriesFast();
 
-   Long64_t nbytes = 0, nb = 0;
-   float nPassTrig = 0;
-   
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
+  Long64_t nbytes = 0, nb = 0;
+  float nPassTrig = 0;
+  
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   nbytes += nb;
 
-      if (HLT_IsoMu24 || HLT_IsoTkMu24)
-	nPassTrig++;
-      
-      // if (Cut(ientry) < 0) continue;
+    if (HLT_IsoMu24 || HLT_IsoTkMu24)
+    nPassTrig++;
+    
+    // if (Cut(ientry) < 0) continue;
    }
+  for (UInt_t i = 0; i < nMuon; i++)
+  {
+    if(MuonSelection(i)) Nu_Mu;
+  }
    
-   cout <<"pass hlt      "<< nPassTrig <<endl;
-   cout <<"total hlt     "<< nentries <<endl;
-   cout <<"pass hlt frac "<< nPassTrig/float(nentries) <<endl;
+  cout <<"pass hlt      "<< nPassTrig <<endl;
+  cout <<"total hlt     "<< nentries <<endl;
+  cout <<"pass hlt frac "<< nPassTrig/float(nentries) <<endl;
 }
 
 int main()
